5d07f9c9971532041d621f93db295e324103d38b
Jacobo <jacobo@Mac-mini-de-Jacobo.local>
2026-04-10 12:01:36 -0600

    Se agrega redireccionamiento al dashboard para que cuando no exista sesion se redireccione al home

--- app-angular/src/app/pages/dashboard/dashboard.ts
+++ app-angular/src/app/pages/dashboard/dashboard.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'dashboard',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css'
 })
-export default class Dashboard { }
+export default class Dashboard {
+  userService = inject(UserService);
+  router = inject(Router)
+
+  constructor(){
+    if(!this.userService.isTokenValid()){
+      this.router.navigate(['/']);
+    }
+  }
+}
